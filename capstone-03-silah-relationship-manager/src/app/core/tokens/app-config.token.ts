1e1b8ffe2e919b8b83ab8416d4623822f32af1ed
MSadawi <sadawe147@gmail.com>
2026-09-01 14:29:52 +0800

    Capstone 03: Switch API to HTTPS

--- capstone-03-silah-relationship-manager/src/app/core/tokens/app-config.token.ts
+++ capstone-03-silah-relationship-manager/src/app/core/tokens/app-config.token.ts
@@ -1,6 +1,6 @@
 import { InjectionToken, Provider } from '@angular/core';
 
-const API_URL = 'http://angular-in-30-days-production.up.railway.app/api'
+const API_URL = 'https://angular-in-30-days-production.up.railway.app/api'
 
 export const apiUrlToken = new InjectionToken<string>('api.url');
 
