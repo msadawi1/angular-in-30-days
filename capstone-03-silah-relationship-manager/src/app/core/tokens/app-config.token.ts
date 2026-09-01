12b5e8ba5c9efb577f88a743027dc47eaffddb6e
MSadawi <sadawe147@gmail.com>
2026-09-01 14:21:26 +0800

    Capstone 03: Change API URL

--- capstone-03-silah-relationship-manager/src/app/core/tokens/app-config.token.ts
+++ capstone-03-silah-relationship-manager/src/app/core/tokens/app-config.token.ts
@@ -1,6 +1,6 @@
 import { InjectionToken, Provider } from '@angular/core';
 
-const API_URL = 'http://localhost:3000/api'
+const API_URL = 'http://angular-in-30-days-production.up.railway.app/api'
 
 export const apiUrlToken = new InjectionToken<string>('api.url');
 
